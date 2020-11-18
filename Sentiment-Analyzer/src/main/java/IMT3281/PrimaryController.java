93aa2a1cea482c5cdd8f39e84e9015eda90fb533
bako1 <abdi@bako.noreply.github.com>
2020-11-18 15:05:57 +0100

    ikke noe nytt

--- Sentiment-Analyzer/src/main/java/IMT3281/PrimaryController.java
+++ Sentiment-Analyzer/src/main/java/IMT3281/PrimaryController.java
@@ -65,6 +65,18 @@ public class PrimaryController {
 
     System.exit(0);
     }
+    Runnable task = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                instruction();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+
+        }
+    };
+
     @FXML
     public void instruction() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
@@ -96,4 +108,6 @@ public class PrimaryController {
 
         bufferedReader.close();
         fileReader.close();
-    }}
+    }
+
+}
