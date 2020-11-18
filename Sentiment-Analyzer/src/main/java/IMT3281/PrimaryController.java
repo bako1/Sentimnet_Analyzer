ce6cc97dbc8954af8cae8cb23f6112298ffcb1df
bako1 <abdi@bako.noreply.github.com>
2020-11-18 02:07:37 +0100

    fjernet mye dupliserte koder

---
+++ Sentiment-Analyzer/src/main/java/IMT3281/PrimaryController.java
@@ -0,0 +1,105 @@
+package IMT3281;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class PrimaryController {
+    @FXML
+    private AnchorPane root;
+    @FXML
+    public static List<File> file;
+
+    private SentimentAnalyser sentenceDetection;
+
+    public PrimaryController(){
+        sentenceDetection = new SentimentAnalyser();
+    }
+
+    @FXML
+    public void multipleFileChooser() throws IOException {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Multiple file");
+        Stage stage = (Stage) root.getScene().getWindow();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("text file", "*.txt"),
+                new FileChooser.ExtensionFilter("doc", "*.doc"),
+                new FileChooser.ExtensionFilter("csv file", "*.csv"));
+        file = fileChooser.showOpenMultipleDialog(stage);
+
+
+        Stage appStage = (Stage) root.getScene().getWindow();
+
+        Parent parent;
+        if (file != null) {
+            if ((long) file.size() > 1) {
+                parent = FXMLLoader.load(getClass().getResource("multipleFile.fxml"));
+                Scene scene = new Scene(parent);
+                appStage.setScene(scene);
+                appStage.show();
+
+            } else if ((long) file.size() == 1) {
+                System.out.println(sentenceDetection.getFileName());
+                parent = FXMLLoader.load(getClass().getResource("singleFile.fxml"));
+                Scene scene = new Scene(parent);
+                appStage.setScene(scene);
+                appStage.show();
+            }
+
+        }else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("No file chosen");
+            alert.show();}
+
+    }
+
+@FXML
+    public void onExit() {
+
+    System.exit(0);
+    }
+    @FXML
+    public void instruction() throws IOException {
+        FileReader fileReader=new FileReader("Sentiment-Analyzer/src/main/resources/IMT3281/instruction");
+        BufferedReader bufferedReader=new BufferedReader(fileReader);
+        List<String>sentences = new ArrayList<>();
+        String words;
+        String str = "";
+        TextArea textArea = new TextArea();
+        textArea.setWrapText(true);
+        textArea.setEditable(false);
+        while ((words = bufferedReader.readLine()) != null) {
+            sentences.add(words);
+
+
+            str+= " "+words+"\n";
+            textArea.setText(str);
+        }
+        for (String s:sentences){
+            System.out.println(s);}
+        Stage newStage = new Stage();
+        Scene scene = new Scene(textArea,400,200);
+        newStage.setTitle("Instructions");
+        newStage.setScene(scene);
+        newStage.show();
+
+
+
+        bufferedReader.close();
+        fileReader.close();
+    }}
---
+++ Sentiment-Analyzer/src/main/java/IMT3281/SingleFileController.java
@@ -0,0 +1,100 @@
+package IMT3281;
+
+import javafx.beans.binding.DoubleBinding;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.net.URL;
+
+import java.util.ResourceBundle;
+
+public class SingleFileController extends PrimaryController implements Initializable {
+
+    @FXML
+    public TableView<Table> tableView;
+    @FXML
+    public TableColumn<Table,String> fileName;
+    @FXML
+    public TableColumn<Table,String> polarity;
+    @FXML
+    public TableColumn<Table,Integer> occurrence;
+    @FXML
+    public TableColumn <Table,String> subject;
+    @FXML
+    public TableColumn<Table, String> sentence;
+    @FXML
+    private  AnchorPane root;
+    public SingleFileController(){}
+
+    @FXML
+    public void onexit( ) {
+        onExit();}
+        @FXML
+    public void goBackToHomePage() throws IOException {
+    Stage appStage;
+    Parent parent;
+    appStage = (Stage) root.getScene().getWindow();
+    parent = FXMLLoader.load(getClass().getResource("primary.fxml"));
+    Scene scene = new Scene(parent);
+    appStage.setScene(scene);
+    appStage.show();
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        fileName.setCellValueFactory(new PropertyValueFactory<>("fileName"));
+        subject.setCellValueFactory(new PropertyValueFactory<>("subject"));
+        occurrence.setCellValueFactory(new PropertyValueFactory<>("occurrence"));
+        polarity.setCellValueFactory(new PropertyValueFactory<>("polarity"));
+        sentence.setCellValueFactory(new PropertyValueFactory<>("sentence"));
+
+        tableView.setItems(getInfo());
+
+    }
+
+    private ObservableList<Table> getInfo() {
+        ReadFiles readFiles = new ReadFiles();
+
+        ObservableList<Table>tableObservableList = FXCollections.observableArrayList();
+        SentimentAnalyser sentimentAnalyser = new SentimentAnalyser();
+        sentimentAnalyser.sentenceRecognizer(readFiles.readFiles(file));
+        Table table = new Table(sentimentAnalyser.getFileName(),sentimentAnalyser.getSubject(),sentimentAnalyser.getPolarity(),sentimentAnalyser.getOccurrence());
+        //for(String s : table.getProcessedSentence())
+           // table.setSentence(s);
+        tableObservableList.add(table);
+        DoubleBinding usedWidth = fileName.widthProperty().add(subject.widthProperty()).add(occurrence.widthProperty());
+
+        sentence.prefWidthProperty().bind(tableView.widthProperty().subtract(usedWidth));        return tableObservableList;
+    }
+@FXML
+    public void browseFiles() throws IOException {
+        multipleFileChooser();
+
+    }
+
+    @FXML
+    private void usage() throws IOException {
+        instruction();
+    }
+}
